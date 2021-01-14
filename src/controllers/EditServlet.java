fafc41c2172690676fac82fc81bfac992ad3a7ac
daiki.nakanishi <dn19951121@icloud.com>
2021-01-14 16:47:04 +0900

    Modify null control

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -25,7 +25,6 @@ public class EditServlet extends HttpServlet {
      */
     public EditServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
     /**
@@ -41,7 +40,9 @@ public class EditServlet extends HttpServlet {
         request.setAttribute("task", m);
         request.setAttribute("_token", request.getSession().getId());
 
-        request.getSession().setAttribute("task_id", m.getId());
+        if(m != null) {
+            request.getSession().setAttribute("task_id", m.getId());
+        }
 
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/edit.jsp");
         rd.forward(request, response);
